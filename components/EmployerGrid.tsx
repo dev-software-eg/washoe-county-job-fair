01f75d4da4eec140efa70764e3acbd3ae6e1d278
justin470 <justin@estiponagroup.com>
2026-07-28 12:33:25 -0700

    Re-order logos

--- components/EmployerGrid.tsx
+++ components/EmployerGrid.tsx
@@ -11,15 +11,15 @@ type Employer = {
 
 const EMPLOYERS: Employer[] = [
   { name: "Carson Valley Health", file: "Carson Valley Health.png", width: 1200, height: 900, padding: "p-0" },
-  { name: "Empire Cat", file: "Empire Cat logo.png", width: 1154, height: 306 },
-  { name: "FedEx Freight", file: "FedexFreight.png", width: 456, height: 272, padding: "p-2" },
-  { name: "Hamilton Company", file: "HAMILTON CO logo.png", width: 596, height: 170, padding: "p-2" },
-  { name: "Nevada Legislature", file: "Legislative Counsel Bureau.png", width: 1280, height: 768, padding: "p-2" },
   { name: "Nevada Gold Mines", file: "Nevada Gold Mines logo.png", width: 565, height: 254 },
   { name: "Truckee Meadows Community College", file: "STACKED TMCC GREEN.png", width: 1009, height: 720, padding: "p-2" },
+  { name: "Nevada Legislature", file: "Legislative Counsel Bureau.png", width: 1280, height: 768, padding: "p-2" },
+  { name: "FedEx Freight", file: "FedexFreight.png", width: 456, height: 272, padding: "p-2" },
   { name: "Panasonic Energy", file: "panasonic logo.png", width: 450, height: 142 },
-  { name: "University of Nevada, Reno", file: "unr-logo.png", width: 2185, height: 1584 },
+  { name: "Hamilton Company", file: "HAMILTON CO logo.png", width: 596, height: 170, padding: "p-2" },
+  { name: "Empire Cat", file: "Empire Cat logo.png", width: 1154, height: 306 },
   { name: "United States Postal Service", file: "usps-8211-united-states-postal-service.png", width: 800, height: 800, padding: "p-0" },
+  { name: "University of Nevada, Reno", file: "unr-logo.png", width: 2185, height: 1584 },
 ];
 
 export default function EmployerGrid() {
